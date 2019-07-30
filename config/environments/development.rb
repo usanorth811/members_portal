74a3d623ba3b00f2978338aeb6ffa3814c7e2364
Caleb Woods <woodscaleb@gmail.com>
2019-07-30 13:11:34 -0700

    turned emails back on

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     domain:               'usan.org', 
-    address:              'usaexch16.usanorth811.org',
+    address:              'usaexch01.usan.org',
     authentication:       'login',
     user_name:            'caleb.woods',
     password:             'Temp1234!',    
