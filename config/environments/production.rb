2a3bf76f68c241a38d1776abdc757630b14d0e44
Caleb Woods <woodscaleb@gmail.com>
2019-07-30 13:20:27 -0700

    changed production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "https://guarded-depths-88265.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "localhost" }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
