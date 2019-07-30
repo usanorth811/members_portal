ef268328ff79214bff8fae5e5a8be1a88f16e9b2
Caleb Woods <woodscaleb@gmail.com>
2019-07-30 08:59:46 -0700

    fixed mobile formatting on forms and buttons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     domain:               'usan.org', 
     address:              'usaexch16.usanorth811.org',
-    port:                 578,
     authentication:       'login',
     user_name:            'caleb.woods',
     password:             'Temp1234!',    
