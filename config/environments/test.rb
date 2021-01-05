5208457856efea69aad779bddae984e5aeb9191e
Caleb Woods <calebwoods@Calebs-MacBook-Pro.local>
2021-01-05 08:04:00 -0800

    removed problematic js

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
 end
