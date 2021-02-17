77daf2c39244093d9e7f95b9c6996d7476fa88c2
Caleb Woods <calebwoods@Calebs-MacBook-Pro.local>
2021-02-17 15:00:44 -0800

    added code status update service

--- app/models/codestatus.rb
+++ app/models/codestatus.rb
@@ -1,4 +1,10 @@
 class Codestatus < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  after_commit :send_update
+
+  def send_update
+    UpdateDetails.new(id).update
+  end
 end
---
+++ app/services/update_details.rb
@@ -0,0 +1,21 @@
+class UpdateDetails
+
+  def initialize(codestatus)
+    @codestatus = Codestatus.find(codestatus)
+    require 'httparty'
+    require 'json'
+    response = HTTParty.get("http://UsanPull1API.usanorth811.org/members?code="+ @codestatus.code, :verify => false)
+    @member_detail = response['data'][0]
+  end
+
+  def update
+    @result = HTTParty.put("http://UsanPull1API.usanorth811.org/members/#{@member_detail['id']}",
+                           :body => {:member => {
+                               :comments => "#{@member_detail['attributes']['comments']} [#{
+                               @codestatus.sar_updated ? "SAR_UPDATED" : "CONTACTS_UPDATED"}: #{@codestatus.created_at.to_s}]"
+                           }}.to_json,
+                           :headers => { 'Content-Type' => 'application/json' } )
+    puts @result.code
+  end
+
+end
