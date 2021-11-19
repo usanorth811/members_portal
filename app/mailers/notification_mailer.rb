d54baef1dfa015fe8b6e60e4e860a67876303421
Caleb Woods <calebwoods@Calebs-MacBook-Pro.local>
2021-11-19 10:30:47 -0800

    fixed redirect issues

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   def new_ticket
     @delivery = Delivery.find(params[:delivery])
     @ticket = ConcernedCitizenTicket.find(params[:ticket])
-    mail(from: 'memberservices@usanorth811.org', to: @delivery.email,
+    mail(from: 'memberservices@em6567.usanorth811.org', to: @delivery.email,
          subject: "New #{@ticket.ticket_type == 'cross_bore' ? 'Cross Bore' : 'Concerned Citizen'} Ticket")
   end
 end
